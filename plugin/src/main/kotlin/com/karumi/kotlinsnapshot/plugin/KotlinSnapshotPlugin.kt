a7d8abc78e34c54474e83b8603331d9f652c1784
Sergio Gutierrez <sergio@gokarumi.com>
2018-08-31 11:51:10 +0200

    Add kotlinsnapshot dependency right from the plugin:

--- plugin/src/main/kotlin/com/karumi/kotlinsnapshot/plugin/KotlinSnapshotPlugin.kt
+++ plugin/src/main/kotlin/com/karumi/kotlinsnapshot/plugin/KotlinSnapshotPlugin.kt
@@ -2,7 +2,30 @@ package com.karumi.kotlinsnapshot.plugin
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.artifacts.DependencyResolutionListener
+import org.gradle.api.artifacts.ResolvableDependencies
 
 class KotlinSnapshotPlugin : Plugin<Project> {
-    override fun apply(target: Project?) {}
+    override fun apply(target: Project?) {
+        target ?: return
+
+        addTasks(target)
+        addKotlinSnapshotDependency(target)
+    }
+
+    private fun addTasks(project: Project) {
+    }
+
+    private fun addKotlinSnapshotDependency(project: Project) {
+        project.gradle.addListener(object : DependencyResolutionListener {
+            override fun beforeResolve(dependencies: ResolvableDependencies?) {
+                val dependency = project.dependencies
+                    .create("com.karumi:kotlinsnapshot:${project.version}")
+                project.dependencies.add("testImplementation", dependency)
+                project.gradle.removeListener(this)
+            }
+
+            override fun afterResolve(dependencies: ResolvableDependencies?) {}
+        })
+    }
 }
