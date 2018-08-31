96d1608d57aacca349ffd0635d5da4f2978a7da3
Sergio Gutierrez <sergio@gokarumi.com>
2018-08-31 11:42:17 +0200

    Create base class and add plugin dependency to project

---
+++ plugin/src/main/kotlin/com/karumi/kotlinsnapshot/plugin/KotlinSnapshotPlugin.kt
@@ -0,0 +1,8 @@
+package com.karumi.kotlinsnapshot.plugin
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class KotlinSnapshotPlugin : Plugin<Project> {
+    override fun apply(target: Project?) {}
+}
